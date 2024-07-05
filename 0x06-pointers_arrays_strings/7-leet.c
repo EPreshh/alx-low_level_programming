398a437b6c106c7db61d25b2c741f3327d5ab2c5
Precious_o <preciousadebola@gmail.com>
2024-07-04 19:24:58 -0700

    Add C function that encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @str: String to encode
+ *
+ * Return: Encoded string
+ */
+char *leet(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		if ((*ptr == 'a') || (*ptr == 'A'))
+		{
+			*ptr = '4';
+		}
+		else if ((*ptr == 'e') || (*ptr == 'E'))
+		{
+			*ptr = '3';
+		}
+		else if ((*ptr == 'o') || (*ptr == 'O'))
+		{
+			*ptr = '0';
+		}
+		else if ((*ptr == 't') || (*ptr == 'T'))
+		{
+			*ptr = '7';
+		}
+		else if ((*ptr == 'l') || (*ptr == 'L'))
+		{
+			*ptr = '1';
+		}
+		ptr++;
+	}
+	return (str);
+}
