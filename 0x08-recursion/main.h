107bbd8e9bc6b4ad9b6186c842703582ab609496
Precious_o <preciousadebola@gmail.com>
2024-07-11 19:32:56 -0700

    Add C function that returns 1 if a string is a palindrome and 0 if not

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,5 +13,8 @@ int _sqrt_recursion(int n);
 int _sqrt_recursive_helper(int n, int start, int end);
 int is_prime_number(int n);
 int _is_prime_recursive(int n, int i);
+int is_palindrome(char *s);
+int _strlen_recursion(char *s);
+int _is_palindrome_recursive(char *s, int start, int end);
 
 #endif /* MAIN_H */
