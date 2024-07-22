b323a49ced39a56af8313c21b1d9f93ee7978f55
Precious_o <preciousadebola@gmail.com>
2024-07-22 16:39:52 -0700

    Add C function-like macro ABS(x) that computes the absolute value of a number x

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,10 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-#define ABS(x) (abs(x))
+#define ABS(x) ((x) < 0 ? -(x) : (x))
 
 #endif /* MAIN_H */
