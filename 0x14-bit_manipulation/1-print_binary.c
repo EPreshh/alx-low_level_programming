58a02bb4c405f17e12e5540467cb9c98c71d538b
Precious_o <preciousadebola@gmail.com>
2024-08-25 17:29:03 -0700

    Fulfill betty checks

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,7 +4,7 @@
  * print_binary - Prints the binary representation of a number.
  * @n: The number to be printed in binary.
  */
-void print_binary(unsigned long int n) 
+void print_binary(unsigned long int n)
 {
 	if (n > 1)
 	{
